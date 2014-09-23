03a76dc9ab81d3a3461d321fa081114a3851f2b8
fastLittleFoxy <saul.a.johnson@gmail.com>
2014-09-24 00:39:34 +0100

    Basic layout.

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+// Imports.
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+/**
+ * Compiles the project's SASS.
+ */
+gulp.task('sass', function() {
+    return gulp.src('css/*.scss')
+        .pipe(sass({ style: 'compressed' }))
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(gulp.dest('css'))
+});
+
+gulp.task('default', ['sass']);
---
+++ index.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Flatfolio - Flat File Portfolio</title>
+    <link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="bower_components/lemonade/css/lemonade.css" />
+    <link rel="stylesheet" type="text/css" href="bower_components/slice/css/slice.css" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+</head>
+<body>
+<div class="frame">
+    <div class="bit-1 logo-holder">
+        <div class="logo">
+            <b>Flat</b>folio - <span>Flat File Portfolio</span>
+        </div>
+    </div>
+</div>
+<div class="frame">
+    <div class="bit-3 gallery-tile">
+        <div class="gallery-tile-caption">Lorem Ipsum</div>
+        <div class="gallery-tile-actions">&#59168; &#59146;</div>
+    </div>
+    <div class="bit-3 gallery-tile">
+        <div class="gallery-tile-caption">Dolor sit Amet</div>
+        <div class="gallery-tile-actions">&#59168; &#59146;</div>
+    </div>
+    <div class="bit-3 gallery-tile">
+        <div class="gallery-tile-caption">Nunce Dest</div>
+        <div class="gallery-tile-actions">&#59168; &#59146;</div>
+    </div>
+</div>
+<div class="frame">
+    <div class="bit-3 gallery-tile">
+        <div class="gallery-tile-caption">Lorem Ipsum</div>
+        <div class="gallery-tile-actions">&#59168; &#59146;</div>
+    </div>
+    <div class="bit-3 gallery-tile">
+        <div class="gallery-tile-caption">Dolor sit Amet</div>
+        <div class="gallery-tile-actions">&#59168; &#59146;</div>
+    </div>
+    <div class="bit-3 gallery-tile">
+        <div class="gallery-tile-caption">Nunce Dest</div>
+        <div class="gallery-tile-actions">&#59168; &#59146;</div>
+    </div>
+</div>
+<div class="frame">
+    <div class="bit-3 gallery-tile">
+        <div class="gallery-tile-caption">Lorem Ipsum</div>
+        <div class="gallery-tile-actions">&#59168; &#59146;</div>
+    </div>
+    <div class="bit-3 gallery-tile">
+        <div class="gallery-tile-caption">Dolor sit Amet</div>
+        <div class="gallery-tile-actions">&#59168; &#59146;</div>
+    </div>
+    <div class="bit-3 gallery-tile">
+        <div class="gallery-tile-caption">Nunce Dest</div>
+        <div class="gallery-tile-actions">&#59168; &#59146;</div>
+    </div>
+</div>
+</body>
+</html>
