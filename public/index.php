44529683abdc8d0340a4bd59da2cc718e4eca2e6
Saul Johnson <saul.a.johnson@gmail.com>
2016-09-06 10:33:04 +0100

    Remove public directory

--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'init.php';
-
-use Repositories\GalleryRepository;
-
-$repo = new GalleryRepository(__DIR__ . '/media');
-
-echo $twig->render('index.html.twig', array('galleries' => $repo->findAll()));
