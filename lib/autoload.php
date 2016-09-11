132e4bb8165e5bcc97412eca34d958cd8fc02abd
Saul Johnson <saul.a.johnson@gmail.com>
2016-09-11 19:52:13 +0100

    Update autoloader

--- lib/autoload.php
+++ lib/autoload.php
@@ -1,24 +1,41 @@
 <?php
-
-/*
- * PSR-0 filesystem autoloader.
+/**
+ * An example of a project-specific implementation.
  *
- * Yeah nothing to see here folks.
+ * After registering this autoload function with SPL, the following line
+ * would cause the function to attempt to load the \Foo\Bar\Baz\Qux class
+ * from /path/to/project/src/Baz/Qux.php:
+ *
+ *      new \Foo\Bar\Baz\Qux;
+ *
+ * @param string $class The fully-qualified class name.
+ * @return void
  */
+spl_autoload_register(function ($class) {
 
-function autoload($className)
-{
-    $className = ltrim($className, '\'');
-    $fileName  = '';
-    $namespace = '';
-    if ($lastNsPos = strripos($className, '\'')) {
-        $namespace = substr($className, 0, $lastNsPos);
-        $className = substr($className, $lastNsPos + 1);
-        $fileName  = str_replace('\'', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    // project-specific namespace prefix
+    $prefix = 'Flatfolio\\';
+
+    // base directory for the namespace prefix
+    $base_dir = __DIR__ . '/Flatfolio/';
+
+    // does the class use the namespace prefix?
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        // no, move to the next registered autoloader
+        return;
     }
-    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
 
-    require $fileName;
-}
+    // get the relative class name
+    $relative_class = substr($class, $len);
 
-spl_autoload_register("autoload");
+    // replace the namespace prefix with the base directory, replace namespace
+    // separators with directory separators in the relative class name, append
+    // with .php
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    // if the file exists, require it
+    if (file_exists($file)) {
+        require $file;
+    }
+});
